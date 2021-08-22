8cc10cc8c88b99eefe928920b82b13334e0ac523
Daniel Mason <danmasson0@gmail.com>
2021-08-22 09:41:19 -0400

    added the ability to visit each product and go back

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-
+const replaceTemplate = require('./module/replaceTemplate');
 
 // //////////////// Files
 // // Blocking, synchronous way
@@ -15,7 +15,6 @@ const url = require('url');
 // const textOut = `this is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
 // fs.writeFileSync('./project/txt/output.txt', textOut);
 // console.log('File Written!');
-
 
 
 // Non-blocking, asynchronous way
@@ -36,23 +35,7 @@ const url = require('url');
 // });
 // console.log('will read file!');
 
-
-
 // //////// Server
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-if(!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
-    return output;
-}
 
 const tempOverview = fs.readFileSync (`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync (`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -66,7 +49,7 @@ const server = http.createServer((req, res) => {
     const {query, pathname} = url.parse(req.url, true);
 
     // Overview page
-    if(pathName === '/' || pathname === '/overview') {
+    if(pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-Type': 'text/html'});
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -76,8 +59,9 @@ const server = http.createServer((req, res) => {
     // Product page
     else if (pathname === '/product') {
         res.writeHead(200, { 'Content-Type': 'text/html'});
-
-        res.end(tempProduct);
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
     }
     // Api
      else if (pathname === '/api') {
